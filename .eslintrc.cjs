72f8de4ec65f6038404ed74ee7240331d0a85e3f
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-31 21:38:01 -0500

    prepare environment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,31 @@
+/* eslint-disable */
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-uses-vars': 'warn'
+  }
+};
