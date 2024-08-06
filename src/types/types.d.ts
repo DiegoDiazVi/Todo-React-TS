97984fb4d2e436feb4fe77b5692ddade999512af
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-05 22:02:57 -0500

    refactor: Remove unused code from types.d.ts

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -6,14 +6,7 @@ export interface Todo {
 }
 
 export type TodoId = Pick<Todo, 'id'>;
-export type TodoTitle = Pick<Todo, 'title'>;
-export type TodoCompleted = Pick<Todo, 'completed'>;
-export type TodoFunctionId = (id: TodoId) => void;
 export type FiltersValue = typeof TODO_FILTERS[keyof typeof TODO_FILTERS];
-
-export interface HeaderProps {
-    onAddTodo: ({ title }: TodoTitle) => void
-}
 
 export interface ActionType {
     type: typeof ACTION_TYPES[keyof typeof ACTION_TYPES];
