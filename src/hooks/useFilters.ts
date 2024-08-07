2cf9541bc9b9aa75d7d375e70be8c2571a8d08a9
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-07 15:00:08 -0500

    refactor: Update useFilters hook to optimize filtering logic and improve performance

--- src/hooks/useFilters.ts
+++ src/hooks/useFilters.ts
@@ -1,7 +1,6 @@
-import { useContext, useEffect, useState, useMemo} from "react";
+import { useContext } from "react";
 import { TaskContext } from "../context/TodoContext";
-import { TODO_FILTERS } from "../constants/constants";
-import type { FiltersValue, FiltersType, Todo } from '../types/types';
+import type { FiltersValue, FiltersType} from '../types/types';
 
 function useFilters(): FiltersType {
     const context = useContext(TaskContext);
@@ -9,49 +8,11 @@ function useFilters(): FiltersType {
         throw new Error('TaskContext debe ser usado dentro de un TodoProvider');
     }
 
-    const { tasks } = context;
-    const [filterSelected, setFilterSelected] = useState<FiltersValue>(TODO_FILTERS.ALL);
-    const [filteredTodos, setFilteredTodos] = useState<Todo[]>(tasks);
+    const { filteredTodos, filterSelected, activeCount, completedCount, setFilterSelected } = context;
 
-    useEffect(() => {
-        const newFilteredTodos = tasks.filter(todo => {
-            switch (filterSelected) {
-            case TODO_FILTERS.ACTIVE:
-                return !todo.completed
-            case TODO_FILTERS.COMPLETED:
-                return todo.completed
-            default:
-                return todo
-            }
-        })
-        setFilteredTodos(newFilteredTodos)
-    }, [filterSelected, tasks])
-
-    /**
-     * Handles the change of the filter value.
-     *
-     * @param filter - The new filter value.
-     */
     const handlerFilterChange = (filter: FiltersValue): void => {
         setFilterSelected(filter);
     }
-
-
-
-
-    /**
-     * The number of active tasks.
-     *
-     * @type {number}
-     */
-    const activeCount: number = useMemo(() => tasks.filter(task => task.completed !== true).length, [tasks]);
-
-    /**
-     * The number of completed tasks.
-     *
-     * @type {number}
-     */
-    const completedCount: number = useMemo(() => tasks.filter(task => task.completed === true).length, [tasks])
 
     return { handlerFilterChange, filteredTodos, activeCount, completedCount, filterSelected }
 }
